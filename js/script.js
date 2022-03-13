4f568061e38eb7f3fc9d3c737cb43a9a1048afcf
Envision192 <envision192@gmail.com>
2022-03-13 16:09:52 +0300

    hw1step

---
+++ js/script.js
@@ -0,0 +1,32 @@
+/*
+let username = null;
+let birthage = null;
+const currentYear = 2022;
+
+username = prompt("Как тебя зовут?");
+alert(`Привет, ${username}`);
+birthAge = prompt("В каком году ты родился?");
+let userAge = currentYear - birthAge;
+alert(`По моим подсчетам тебе ${userAge} !`);
+*/
+
+/*
+let squareLength = prompt("Введите длину стороны квадрата");
+let squarePer = squareLength * 4;
+alert(`Площадь квадрата ${squarePer}`);
+*/
+
+/*
+const p = 3.14;
+let userRadius = prompt("Введите радиус окружности");
+let powRadius = Math.pow(userRadius, 2);
+let sRound = p * powRadius;
+alert(`Радиус окружности будет равен ${sRound} !`);
+*/
+
+/*
+let distance = prompt("Введите расстояние между двумя городами");
+let time = prompt("Введите время за которое вы хотите добраться");
+let speed = distance / time;
+alert(`Скорость с которой вам необходимо двигаться ${speed} км/ч !`);
+*/
